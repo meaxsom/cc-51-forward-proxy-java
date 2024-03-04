126e96b8255a76b38534031b75aab102e0626d5e
Matthew E. Axsom <git@tipsydeveloper.io>
2024-03-04 11:07:30 -0500

    added null handling for list

--- app/src/main/java/com/gruecorner/forwardproxy/HttpProxyRequest.java
+++ app/src/main/java/com/gruecorner/forwardproxy/HttpProxyRequest.java
@@ -22,27 +22,31 @@ public class HttpProxyRequest {
     private Map<String, String> m_headers = new HashMap<String, String>();
 
     public HttpProxyRequest(List<String> inRequest) {
-        int theRequestSize = inRequest.size();
+        if (inRequest != null)
+            {
+            int theRequestSize = inRequest.size();
 
-        if (theRequestSize > 0) {
-            String[] theLineParts = inRequest.get(0).split("\\s+");
-            if (theLineParts.length == 3) {
-                m_method = theLineParts[0].trim();
-                m_requst = theLineParts[1].trim();
-                m_protocol = theLineParts[2].trim();
-            
-                if (theRequestSize > 1) {
-                    for (String theLine : inRequest.subList(1, inRequest.size())) {
-                        String[] theHeaderLine = theLine.split("\\:\\s+");
-                        if (theHeaderLine.length == 2)
-                            m_headers.put(theHeaderLine[0], theHeaderLine[1]);
-                    }
+            if (theRequestSize > 0) {
+                String[] theLineParts = inRequest.get(0).split("\\s+");
+                if (theLineParts.length == 3) {
+                    m_method = theLineParts[0].trim();
+                    m_requst = theLineParts[1].trim();
+                    m_protocol = theLineParts[2].trim();
+                
+                    if (theRequestSize > 1) {
+                        for (String theLine : inRequest.subList(1, inRequest.size())) {
+                            String[] theHeaderLine = theLine.split("\\:\\s+");
+                            if (theHeaderLine.length == 2)
+                                m_headers.put(theHeaderLine[0], theHeaderLine[1]);
+                        }
+                    } else
+                        kLogger.info("Seem's odd there are no headers");
                 } else
-                    kLogger.info("Seem's odd there are no headers");
+                    kLogger.error("Request is invalid");
             } else
-                kLogger.error("Request is invalid");
+                kLogger.error("Request is empty!");
         } else
-            kLogger.error("Request is empty!");
+            kLogger.error("Request is null!");
     }
 
     public String getMethod() {
