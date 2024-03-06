33ab3a39b789e3d52e7944f68ac5a7faf8c92971
Matthew E. Axsom <git@tipsydeveloper.io>
2024-03-05 19:43:44 -0500

    Banned hosts read in

---
+++ app/src/test/java/com/gruecorner/forwardproxy/utils/CommandLineHelperTest.java
@@ -0,0 +1,30 @@
+package com.gruecorner.forwardproxy.utils;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.util.Set;
+
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.core.config.Configurator;
+import org.apache.logging.log4j.core.config.DefaultConfiguration;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CommandLineHelperTest {
+    private final static String kBannedFilePath = "src/test/data/banned-hosts.txt";
+
+    @Before
+    public void setUp() throws Exception {
+        Configurator.initialize(new DefaultConfiguration());
+        Configurator.setRootLevel(Level.DEBUG);
+    }
+
+    @Test
+    public void testConfigureBannedHosts() {
+        CommandLineHelper.configureBannedHosts(kBannedFilePath);
+        Set<String> theSet = CommandLineHelper.getBannedHosts();
+        assertNotNull(theSet);
+        assertEquals(theSet.size(), 3);
+    }
+}
