d72bab9107450ffb27b663222fdcc7405a6d9536
Matthew E. Axsom <git@tipsydeveloper.io>
2024-03-05 19:43:44 -0500

    Banned hosts read in

--- app/src/main/java/com/gruecorner/forwardproxy/utils/CommandLineHelper.java
+++ app/src/main/java/com/gruecorner/forwardproxy/utils/CommandLineHelper.java
@@ -2,7 +2,11 @@ package com.gruecorner.forwardproxy.utils;
 
 import gnu.getopt.Getopt;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.util.HashSet;
+import java.util.Set;
 
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
@@ -14,19 +18,23 @@ import org.apache.logging.log4j.core.config.DefaultConfiguration;
 public class CommandLineHelper {
 	private final static Logger kLogger =	LogManager.getLogger(CommandLineHelper.class.getName());
 
-	private final static String kHelpString = "-l {log4j-config} -  path to log4j config file\n"
-		+ "-p {port} forward proxy port number\n"
-		+ "-t {thread-count} number of threads to use\n"
-		+ "-h - print help.. all other options are ignored and program terminates\n";
-
-    private static String	kDefaultOptions			= 	":hp:l:t:";
+    private static String	kDefaultOptions			= 	":hp:l:t:b:";
 	private static int		kDefaultPort			=	8989;
 	private static int		kDefaultThreadPoolSize	=	100;
 
-	private static int		s_port 				= kDefaultPort;
-	private static int		s_threadPoolSize	= kDefaultThreadPoolSize;
-	private static String	s_log4JConf			= null;
-	private static boolean	s_help 				= false;
+	private final static String kHelpString = "-l {log4j-config} -  path to log4j config file\n"
+		+ "-p {port} optional forward proxy port numberm default is " + kDefaultPort + "\n"
+		+ "-t {thread-count} optional number of threads to use; defaults to " + kDefaultThreadPoolSize + "\n"
+		+ "-b {banned-hosts} optional text file of banned hosts, 1 per line"
+		+ "-h prints this help.. all other options are ignored and program terminates\n";
+
+
+	private static int			s_port 				= kDefaultPort;
+	private static int			s_threadPoolSize	= kDefaultThreadPoolSize;
+	private static String		s_log4JConf			= null;
+	private static String		s_bannedHostsFile	= null;
+	private static boolean		s_help 				= false;
+	private static Set<String>	s_bannedHosts		= new HashSet<String>();
 
 	public static void processArguments(String inProcessName, String inArgs[]) {
 		CommandLineHelper.processArguments(inProcessName, inArgs, kDefaultOptions);
@@ -37,26 +45,30 @@ public class CommandLineHelper {
 		int theOption;
 		while ((theOption = theOpts.getopt()) != -1) {
 			switch (theOption) {
-				case 'p':
-					setPort(theOpts.getOptarg());
+				case 'b':
+					setBannedHostsFile(theOpts.getOptarg());
 					break;
-			
+
 				case 'h':
 					setHelp(true);
+					break;
+
+				case 'l':
+					setLog4JConf(theOpts.getOptarg());
+					break;
+
+				case 'p':
+					setPort(theOpts.getOptarg());
 					break;
 
 				case 't':
 					setThreadPoolSize(theOpts.getOptarg());
 					break;
-					
-				case 'l':
-					setLog4JConf(theOpts.getOptarg());
-					break;
             }
         }
 
+		// process logger here so we can use the logging system ASAP
 		if (s_log4JConf != null) {
-			// replaces DOMConfigurator.configure(s_log4JConf);
 			File file = new File(s_log4JConf);
 			LoggerContext context = (LoggerContext) LogManager.getContext(false);
 			context.setConfigLocation(file.toURI());
@@ -73,7 +85,7 @@ public class CommandLineHelper {
 	}
 
 	public static void setPort(int inPort) {
-		CommandLineHelper.s_port = inPort;
+		s_port = inPort;
 	}
 
 	public static void setPort(String inPort) {
@@ -81,7 +93,7 @@ public class CommandLineHelper {
 			CommandLineHelper.setPort(Integer.parseInt(inPort));
 		} catch (Throwable theErr) {
 			kLogger.error("Can't set port to: " + inPort + ". Using default of " + kDefaultPort, theErr);
-			CommandLineHelper.s_port = kDefaultPort;
+			s_port = kDefaultPort;
 		}
 	}
 
@@ -90,23 +102,20 @@ public class CommandLineHelper {
 	}
 
 	public static void setLog4JConf(String inLog4JConf) {
-		CommandLineHelper.s_log4JConf = inLog4JConf;
+		s_log4JConf = inLog4JConf;
 	}
 
-	public static String getHelp()
-		{
+	public static String getHelp() {
 		return kHelpString;
-		}
+	}
 
-	public static void setHelp(boolean inHelp)
-		{
-		CommandLineHelper.s_help = inHelp;
-		}
+	public static void setHelp(boolean inHelp) {
+		s_help = inHelp;
+	}
 		
-	public static boolean isHelp()
-		{
+	public static boolean isHelp() {
 		return s_help;
-		}
+	}
 
 	public static int getThreadPoolSize() {
 		return s_threadPoolSize;
@@ -118,11 +127,41 @@ public class CommandLineHelper {
 
 	public static void setThreadPoolSize(String inThreadPoolSize) {
 		try {
-
+			setThreadPoolSize(Integer.valueOf(inThreadPoolSize));
 		} catch (Throwable theErr) {
 			kLogger.error("Can't set pool size to: " + inThreadPoolSize + ". Using default of " + kDefaultThreadPoolSize, theErr);
-			CommandLineHelper.s_threadPoolSize = kDefaultThreadPoolSize;
+			setThreadPoolSize(kDefaultThreadPoolSize);
 		}
 	}
+	public static Set<String> getBannedHostsFile() {
+		return s_bannedHosts;
+	}
 
+	public static void setBannedHostsFile(String inBannedHostsFile) {
+		s_bannedHostsFile = inBannedHostsFile;
+	}
+
+	public static Set<String> getBannedHosts() {
+		return s_bannedHosts;
+	}
+
+	public static void configureBannedHosts(String inBannedHostFile) {
+		File theFile = new File(inBannedHostFile);
+		if (theFile.exists()) {
+			try {
+				BufferedReader theReader = new BufferedReader(new FileReader(theFile));
+
+				String theLine = theReader.readLine();
+				while (theLine != null) {
+					s_bannedHosts.add(theLine);
+					theLine = theReader.readLine();
+				}
+				theReader.close();
+
+			} catch (Throwable theErr) {
+				kLogger.error("Can't read banned host file", theErr);
+			}
+		} else
+			kLogger.error(theFile.getAbsolutePath() + " does not exist. No site banning will be enforced");
+	}
 }
