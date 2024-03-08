7ba09102a33b29cbb747dd4991d4dc4742dafbde
Matthew E. Axsom <git@tipsydeveloper.io>
2024-03-08 16:45:22 -0500

    Support CONNECT/HTTPS proxying

--- app/src/main/java/com/gruecorner/forwardproxy/ProxyHandler.java
+++ app/src/main/java/com/gruecorner/forwardproxy/ProxyHandler.java
@@ -2,6 +2,7 @@ package com.gruecorner.forwardproxy;
 
 import com.gruecorner.forwardproxy.utils.AccessLogger;
 import com.gruecorner.forwardproxy.utils.HttpReply;
+import com.gruecorner.forwardproxy.utils.StreamClient;
 import com.gruecorner.forwardproxy.utils.HttpReply.ResponseCode;
 
 import java.io.IOException;
@@ -14,8 +15,10 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
 import java.net.InetAddress;
+import java.net.Socket;
 import java.net.URI;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -30,7 +33,10 @@ public class ProxyHandler {
     private final static String kXForwardedForHeaderKey     = "X-Forwarded-For";
     private final static String kDefaultFowardAddress       = "127.0.0.1";
     private final static String kUserAgentHeaderKey         = "User-Agent";
-    private final static String kUserAgentHeaderValue       = "cc-51-proxy";
+    private final static String kUserAgentHeaderValue       = "cc-51-proxy/0.5.0";
+
+    private final static String kProxyAgentHeaderKey         = "Proxy-Agent";
+    private final static String kHttpMethodConnect          = "CONNECT";
 
     private static List<String> s_bannedHosts          = null;
     private static List<String> s_bannedWords          = null;
@@ -51,20 +57,27 @@ public class ProxyHandler {
                 theProxyRequest.setInetAddress(inAddress);
                 theProxyRequest.setPort(inPort);
 
-                Optional<HttpResponse<String>> theResponseOptional = proxyRequest(theProxyRequest);
-                if (theResponseOptional.isPresent()) {
-                    HttpResponse<String> theResponse = theResponseOptional.get();
-                    if (theResponse.statusCode() == 200) {
-                        // check for banned words in the response body
-                        if (isWordsAllowed(theResponse.body()))
-                            handleResponse(theProxyRequest, theResponse, inOutputStream);
-                        else
-                            handleInvalidResponse(theProxyRequest, inOutputStream, HttpReply.ResponseCode.Forbidden, "Website content not allowed.");
+                // decide if this is a connect request for a tunnel or something more normal
+                if (kHttpMethodConnect.equalsIgnoreCase(theProxyRequest.getMethod())) {
+                    tunnelRequest(theProxyRequest, inInputStream, inOutputStream);
+                }
+                else { // normal stuff
+                    Optional<HttpResponse<String>> theResponseOptional = proxyRequest(theProxyRequest);
+                    if (theResponseOptional.isPresent()) {
+                        HttpResponse<String> theResponse = theResponseOptional.get();
+                        if (theResponse.statusCode() == 200) {
+                            // check for banned words in the response body
+                            if (isWordsAllowed(theResponse.body()))
+                                handleResponse(theProxyRequest, theResponse, inOutputStream);
+                            else
+                                handleInvalidResponse(theProxyRequest, inOutputStream, HttpReply.ResponseCode.Forbidden, "Website content not allowed.");
+                        }
+                        else // should try to pass back a response code that matches the original
+                            handleInvalidResponse(theProxyRequest, inOutputStream, HttpReply.ResponseCode.BadRequest, "Host returned: " + theResponse.statusCode());
+                    } else {
+                       handleInvalidResponse(theProxyRequest, inOutputStream, HttpReply.ResponseCode.BadRequest, "No response from host");
                     }
-                    else // should try to pass back a response code that matches the original
-                        handleInvalidResponse(theProxyRequest, inOutputStream, HttpReply.ResponseCode.BadRequest, "Host returned: " + theResponse.statusCode());
-                } else
-                    handleInvalidResponse(theProxyRequest, inOutputStream, HttpReply.ResponseCode.BadRequest, "No response from host");
+                }
             } else
                 handleInvalidResponse(theProxyRequest, inOutputStream, HttpReply.ResponseCode.Forbidden, "Website not allowed: " + theProxyRequest.getRequst());
         } else {
@@ -73,6 +86,51 @@ public class ProxyHandler {
         }
         
         inOutputStream.close();
+    }
+
+    public void tunnelRequest(HttpProxyRequest inProxyRequest, InputStream inInputStream, OutputStream inOutputStream) {
+        // build the reply to the client for establising the tunnel
+        HttpReply theReply = HttpReply.Builder.newInstance()
+        .setResponseCode(HttpReply.ResponseCode.ProxyConnectionOK)
+        .addHeader(kProxyAgentHeaderKey, kUserAgentHeaderValue)
+        .build();
+
+        try {
+            try {
+                URL theUrl = new URL(inProxyRequest.getRequst());
+                
+                // get the kind of socket we'll need - it'll do the right thing based on the protocol in the host
+                Socket theTunnel = new Socket(theUrl.getHost(), theUrl.getPort());
+
+                // create different streamers.. one for each direction
+                StreamClient theServerStream = new StreamClient("client->server", inInputStream, theTunnel.getOutputStream());
+                StreamClient theClientStream = new StreamClient("server->client", theTunnel.getInputStream(), inOutputStream);
+ 
+                // keep the list of streamers so we can check their "join" status later
+                List<StreamClient> theClients = new ArrayList<>();
+                theClients.add(theServerStream);
+                theClients.add(theClientStream);
+
+                // start the streams, send the notification to start and wait for the threads to join
+                theServerStream.start();
+                theClientStream.start();
+
+                // reply to the client that we're ready with the tunnel
+                theReply.send(inOutputStream);
+
+                // wait for all clients to finish
+                for (StreamClient theClient : theClients)
+                    theClient.join();
+
+                // close the tunnel socket
+                theTunnel.close();
+
+            } catch(Throwable theErr) {
+                kLogger.error("Error with tunnel", theErr);
+            }
+        } catch (Throwable theErr) {
+            kLogger.error("Error establishing proxy tunnel", theErr);
+        }
     }
 
     public Optional<HttpResponse<String>> proxyRequest(HttpProxyRequest inProxyRequest) {
@@ -97,13 +155,13 @@ public class ProxyHandler {
         try {
             // report on the request.. should go to configured logger
             AccessLogger.log("Client: " + inProxyRequest.getInetAddressString() + " Request URL: " + inProxyRequest.getRequst());
- 
+
             HttpResponse<String> theResponse = theClient.send(theHttpRequest, BodyHandlers.ofString());
             result = Optional.of(theResponse);
         } catch(Throwable theErr) {
             kLogger.error("Error making request", theErr);
         }
-        
+
     return result;
     }
 
@@ -131,6 +189,7 @@ public class ProxyHandler {
             handleInvalidResponse(inRequest, inOutputStream, HttpReply.ResponseCode.BadRequest, "Bad Request...");
     }
 
+    // write back an error to the client
     public void handleInvalidResponse(HttpProxyRequest inRequest, OutputStream inOutputStream, ResponseCode inResponseCode, String inMessage) throws IOException {
         HttpReply theNotFoundReply = HttpReply.Builder.newInstance()
             .setBody(inMessage)
@@ -154,7 +213,7 @@ public class ProxyHandler {
                 result = isAllowed(theUrl.getHost(), inBannedHosts) && isAllowed(theUrl.getHost(), inBannedWords);
 
         } catch(Throwable theErr) {
-            kLogger.error("can't evaluate if host " + inRequest.getRequst() + " is allowed, banning...");
+            kLogger.error("can't evaluate if host " + inRequest.getRequst() + " is allowed, banning...", theErr);
             result=false;
         }
         return result;
